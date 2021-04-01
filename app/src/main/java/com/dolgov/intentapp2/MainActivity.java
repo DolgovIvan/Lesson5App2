fa4f53300c325cf39735053c613d6d259fe66a4d
[Dolgov] <[popded51@gmail.com]>
2021-04-02 00:01:21 +0300

    Lesson5App2

--- app/src/main/java/com/dolgov/intentapp2/MainActivity.java
+++ app/src/main/java/com/dolgov/intentapp2/MainActivity.java
@@ -2,7 +2,11 @@ package com.dolgov.intentapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOpen = findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("example://intent");
+                Intent openIntent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(openIntent);
+            }
+        });
     }
 }
